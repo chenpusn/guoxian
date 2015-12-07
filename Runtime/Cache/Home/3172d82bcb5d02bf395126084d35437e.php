a53fef1783148e7693afc4f890eb2665a8edcb45
root <root@haozhixian.novalocal>
2015-12-07 16:28:22 +0800

    install framework

--- Runtime/Cache/Home/3172d82bcb5d02bf395126084d35437e.php
+++ Runtime/Cache/Home/3172d82bcb5d02bf395126084d35437e.php
@@ -42,81 +42,77 @@ var  UPLOAD_FILE = "<?php echo U('File/upload',array('session_id'=>session_id())
 <body>
 	<!-- 头部 -->
 	<style type="text/css">
-  .public_title{
-    position: relative;
-    top: -18px;
-    color: #666;
-    font-size: 18px;
-    left: 10px;
-  }
+    .public_title {
+        position: relative;
+        top: -18px;
+        color: #666;
+        font-size: 18px;
+        left: 10px;
+    }
 </style>
 
 <!-- 提示 -->
 <div id="top-alert" class="top-alert-tips alert-error" style="display: none;">
-  <a class="close" href="javascript:;"><b class="fa fa-times-circle"></b></a>
-  <div class="alert-content">这是内容</div>
+    <a class="close" href="javascript:;"><b class="fa fa-times-circle"></b></a>
+
+    <div class="alert-content">这是内容</div>
 </div>
 <!-- 导航条
 ================================================== -->
 <div class="navbar navbar-inverse navbar-fixed-top">
- 	<?php if ($mid ) { $link = M ( 'public_link' )->where ( 'uid=' . $mid )->order ( 'is_use desc' )->select (); foreach ( $link as $l ) { $mp_ids [] = $l ['mp_id']; $is_use [$l ['mp_id']] = $l ['is_use']; } $mp_ids = getSubByKey ( $link, 'mp_id' ); if (! empty ( $mp_ids )) { $mp_ids_list = count ( $mp_ids ); $mp_ids = implode ( ',', $mp_ids ); $map ['id'] = array ( 'in', $mp_ids ); $public_list = M ( 'public' )->where ( $map )->order ( 'FIND_IN_SET(id,"' . $mp_ids . '")' )->select (); $public = $public_list [0]; $token = get_token (); if ($public ['public_id'] && ($is_use [$public ['id']] == 0 || $token == '' || $token == - 1)) { get_token ( $public ['public_id'] ); } unset ( $public_list [0] ); } else { $mp_ids_list=0; } $publicInfo=M('public')->where(array('uid'=>$mid))->find(); $userInfo=getUserInfo($mid); $nowPublicInfo = M('public')->where(array('token'=>get_token()))->find(); } ?>
+    <?php if ($mid ) { $link = M ( 'public_link' )->where ( 'uid=' . $mid )->order ( 'is_use desc' )->select (); foreach ( $link as $l ) { $mp_ids [] = $l ['mp_id']; $is_use [$l ['mp_id']] = $l ['is_use']; } $mp_ids = getSubByKey ( $link, 'mp_id' ); if (! empty ( $mp_ids )) { $mp_ids_list = count ( $mp_ids ); $mp_ids = implode ( ',', $mp_ids ); $map ['id'] = array ( 'in', $mp_ids ); $public_list = M ( 'public' )->where ( $map )->order ( 'FIND_IN_SET(id,"' . $mp_ids . '")' )->select (); $public = $public_list [0]; $token = get_token (); if ($public ['public_id'] && ($is_use [$public ['id']] == 0 || $token == '' || $token == - 1)) { get_token ( $public ['public_id'] ); } unset ( $public_list [0] ); } else { $mp_ids_list=0; } $publicInfo=M('public')->where(array('uid'=>$mid))->find(); $userInfo=getUserInfo($mid); $nowPublicInfo = M('public')->where(array('token'=>get_token()))->find(); } ?>
     <div class="wrap">
-    
-       <a class="brand" title="<?php echo C('WEB_SITE_TITLE');?>" href="<?php echo U('index/index');?>">
-       <?php if($nowPublicInfo['headface_url'] != NULL): ?><img height="52" src="<?php echo (get_cover_url($nowPublicInfo["headface_url"])); ?>"/>
-       	<?php else: ?>
-       		<img height="52" src="/Public/Home/images/logo.png"/><?php endif; ?>
-      <span class="public_title"><?php echo empty($page_title) ? C('WEB_SITE_TITLE') : $nowPublicInfo['public_name']; ?></span>
-       </a>
-       
-       
-            <div class="top_nav">
-                <?php if(is_login()): ?><ul class="nav" style="margin-right:0">
-                    	<?php if($myinfo["is_init"] == 0 ): ?><li><p>该账号配置信息尚未完善，功能还不能使用</p></li>
-                    		<?php elseif($myinfo["is_audit"] == 0 and !$reg_audit_switch): ?>
-                    		<li><p>该账号配置信息已提交，请等待审核</p></li>
-                    		<?php else: ?> 
-                    		<?php if(is_array($core_top_menu)): $i = 0; $__LIST__ = $core_top_menu;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$ca): $mod = ($i % 2 );++$i;?><li data-id="<?php echo ($ca["id"]); ?>" class="<?php echo ($ca["class"]); ?>"><a href="<?php echo ($ca["url"]); ?>" target="_blank"><?php echo ($ca["title"]); ?></a></li><?php endforeach; endif; else: echo "" ;endif; endif; ?>
-                    	
-                    	
-                        
-                        <li class="dropdown admin_nav">
-                            <a href="#" class="dropdown-toggle login-nav" data-toggle="dropdown" style="">
-                                <?php if(!empty($userInfo[headface_url])): ?><img class="admin_head" src="<?php echo (get_cover_url($userInfo["headface_url"])); ?>"/>
-                                <?php else: ?>
-                                    <img class="admin_head" src="/Public/Home/images/default.png"/><?php endif; ?>
-                                <?php echo (getShort(get_username($mid),4)); ?><b class="pl_5 fa fa-sort-down"></b>
-                            </a>
-                            <ul class="dropdown-menu" style="display:none">
-                            	<li><a href="<?php echo U ('Home/Public/add',array('id'=>$publicInfo[id]));?>">账号配置</a></li>
-                                <li><a href="<?php echo U ('Home/Public/lists');?>">公众号配置</a></li>
-                                <li><a href="<?php echo U('User/profile');?>">修改密码</a></li>
-                                <li><a href="<?php echo U('User/logout');?>">退出</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+        <a class="brand" title="<?php echo C('WEB_SITE_TITLE');?>" href="<?php echo U('index/index');?>">
+            <?php if($nowPublicInfo['headface_url'] != NULL): ?><img height="52" src="<?php echo (get_cover_url($nowPublicInfo["headface_url"])); ?>"/>
                 <?php else: ?>
-                    <ul class="nav" style="margin-right:0">
-                    	<li style="padding-right:20px">你好!欢迎来到<?php echo C('WEB_SITE_TITLE');?></li>
-                        <li>
-                            <a href="<?php echo U('User/login');?>">登录</a>
-                        </li>
-                        <li>
-                            <a href="<?php echo U('User/register');?>">注册</a>
-                        </li>
-                        <li>
-                            <a href="<?php echo U('admin/index/index');?>" style="padding-right:0">后台入口</a>
-                        </li>
-                    </ul><?php endif; ?>
-            </div>
+                <img height="52" src="/Public/Home/images/logo.png"/><?php endif; ?>
+            <span class="public_title"><?php echo empty($page_title) ? C('WEB_SITE_TITLE') : $nowPublicInfo['public_name']; ?></span>
+        </a>
+        <div class="top_nav">
+            <?php if(is_login()): ?><ul class="nav" style="margin-right:0">
+                    <?php if($myinfo["is_init"] == 0 ): ?><li><p>该账号配置信息尚未完善，功能还不能使用</p></li>
+                        <?php elseif($myinfo["is_audit"] == 0 and !$reg_audit_switch): ?>
+                        <li><p>该账号配置信息已提交，请等待审核</p></li>
+                        <?php else: ?>
+                        <?php if(is_array($core_top_menu)): $i = 0; $__LIST__ = $core_top_menu;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$ca): $mod = ($i % 2 );++$i;?><li data-id="<?php echo ($ca["id"]); ?>" class="<?php echo ($ca["class"]); ?>"><a href="<?php echo ($ca["url"]); ?>"
+                                                                          target="_blank"><?php echo ($ca["title"]); ?></a></li><?php endforeach; endif; else: echo "" ;endif; endif; ?>
+                    <li class="dropdown admin_nav">
+                        <a href="#" class="dropdown-toggle login-nav" data-toggle="dropdown" style="">
+                            <?php if(!empty($userInfo[headface_url])): ?><img class="admin_head" src="<?php echo (get_cover_url($userInfo["headface_url"])); ?>"/>
+                                <?php else: ?>
+                                <img class="admin_head" src="/Public/Home/images/default.png"/><?php endif; ?>
+                            <?php echo (getShort(get_username($mid),4)); ?><b class="pl_5 fa fa-sort-down"></b>
+                        </a>
+                        <ul class="dropdown-menu" style="display:none">
+                            <li><a href="<?php echo U ('Home/Public/add',array('id'=>$publicInfo[id]));?>">账号配置</a></li>
+                            <li><a href="<?php echo U ('Home/Public/lists');?>">公众号配置</a></li>
+                            <li><a href="<?php echo U('User/profile');?>">修改密码</a></li>
+                            <li><a href="<?php echo U('User/logout');?>">退出</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <?php else: ?>
+                <ul class="nav" style="margin-right:0">
+                    <li style="padding-right:20px">你好!欢迎来到<?php echo C('WEB_SITE_TITLE');?></li>
+                    <li>
+                        <a href="<?php echo U('User/login');?>">登录</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo U('User/register');?>">注册</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo U('admin/index/index');?>" style="padding-right:0">后台入口</a>
+                    </li>
+                </ul><?php endif; ?>
         </div>
+    </div>
 </div>
 
 <script type="text/javascript">
-  $(function(){
-    $("a[href='http://demo.idouly.com/weiphp3.0/index.php?s=/home/index/main.html&mdm=172']").attr("target","_self");
-   
-  })
+    $(function () {
+        $("a[href='http://demo.idouly.com/weiphp3.0/index.php?s=/home/index/main.html&mdm=172']").attr("target", "_self");
+
+    })
 </script>
 	<!-- /头部 -->
 	
