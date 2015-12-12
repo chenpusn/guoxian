9a720721ed50bd98555967f9d81c4ed2428a661f
chenpusn <624612156@qq.com>
2015-12-12 11:16:10 +0800

    重构用户Mobile端页面及逻辑

--- Addons/Shop/Controller/UserController.class.php
+++ Addons/Shop/Controller/UserController.class.php
@@ -7,19 +7,12 @@ use Home\Controller\AddonsController;
 class MobileController extends AddonsController
 {
     var $userInfo;
-    function _initialize()
-    {
-        parent::_initialize();
-
-        _checkUserBindInfo();
-    }
-
     function _checkUserBindInfo(){
         // 本地未绑定用户信息 如果是需要用户信息的操作 则转至用户新绑定页面
         if(!cookie("HZXUSER".C('SITE_VERSION'))){
             $actionsNeedLogin = array('basket');
             if (in_array(_ACTION, $actionsNeedLogin)){
-                $this->redirect("bindUserInfo");
+                $this->redirect("bind_user");
             }
         }
         // 获取用户信息
@@ -28,6 +21,14 @@ class MobileController extends AddonsController
             $userInfo = json_decode ($userInfoJson);
         }
     }
+
+    function _initialize()
+    {
+        parent::_initialize();
+
+        _checkUserBindInfo();
+    }
+
 
     // CHEN PU: 2015/12/12 移动端 首页
     // 显示商品列表
