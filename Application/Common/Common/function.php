5a8f26bd10a52852b29a080d662170663de32772
chenpusn <624612156@qq.com>
2015-12-28 14:22:55 +0800

    增加短信通知

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -3651,7 +3651,7 @@ function sendSMS($mobile, $content){
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_URL,$url);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-	//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //如果需要将结果直接返回到变量里，那加上这句。
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //如果需要将结果直接返回到变量里，那加上这句。
 	$result = curl_exec($ch);
 	return $result;
 }
